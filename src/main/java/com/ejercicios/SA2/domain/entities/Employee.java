5faea3b9e5968abfcae9018b59e10ec6ee84a42c
david.tainta <david.tainta@bosonit.com>
2022-05-18 11:53:36 +0200

    start SA2

---
+++ src/main/java/com/ejercicios/SA2/domain/entities/Employee.java
@@ -0,0 +1,10 @@
+package com.ejercicios.SA2.domain.entities;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class Employee {
+
+    String name;
+    MultipartFile document;
+
+}
